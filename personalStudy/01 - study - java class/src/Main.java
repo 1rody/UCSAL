8769a5c2e51adbc79ab9048dedcd1b83f942c38a
Rody <131989043+1rody@users.noreply.github.com>
2026-07-30 23:53:55 -0300

    <feat>(main) 1 vez utilizando classes em java

---
+++ personalStudy/01 - study - java class/src/Main.java
@@ -0,0 +1,15 @@
+
+public class Main {
+    public static void main(String[] args) {
+        pessoa pessoa1 = new pessoa();
+        pessoa1.name = "Rodolfo";
+        pessoa1.age = 20;
+        pessoa1.finalName = "falcao";
+        pessoa1.height = 1.67;
+        pessoa1.weight = 45;
+        pessoa1.saudacao = "Ola mundo!";
+
+        pessoa1.presentation();
+
+    }
+}
---
+++ personalStudy/01 - study - java class/src/pessoa.java
@@ -0,0 +1,16 @@
+public class pessoa {
+    String name;
+    String finalName;
+    int age;
+
+    double height;
+    double weight;
+
+    String saudacao;
+    public void presentation() {
+        System.out.println("Ola, eu me chamo" + name + "e tenho " + age + " Anos!");
+        System.out.println("Precisa de algo? qualquer coisa pode me chamar");
+        System.out.println(saudacao);
+    }
+
+}
